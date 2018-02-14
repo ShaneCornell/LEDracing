e00a4297e8eab4d1e98ec6762dd8afbf4313e714
ShaneCornell <35901126+ShaneCornell@users.noreply.github.com>
2018-02-13 20:42:49 -0700

    Add files via upload

--- racingLED.py
+++ racingLED.py
@@ -1,19 +1,24 @@
-# Racing LEDs with buttons to change direction and frequency
+# Racing LEDs with buttons to change direction and freque
 
 from time import sleep
-import RPI.GPIO as GPIO
+from gpiozero import LED
+import RPi.GPIO as gpiozero
 
-#Button setup
-GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-#LED setup
-GPIO.setup(27, GPIO.OUT)
-GPIO.output(27,0)
+led1 = LED(5)
+led2 = LED(6)
+led3 = LED(13)
+led4 = LED(19)
+led5 = LED(26)
+led6 = LED(9)
 
 
-while True:
-    if(GPIO.input(22) == 1):
-        GPIO.output(27,1)
-    else:
-        GPIO.output(27,0)
-        
+
+def forward_racing():
+    x = 0
+    for x in range(1, 6):
+        ledx.on()
+        sleep(t)
+        ledx.off()
